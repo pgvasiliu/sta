9cdafc241fe37f0edc85af2307bee8a3128303d0
George Vasiliu <pg.vasiliu@gmail.com>
2023-12-10 21:23:09 -0500

    update again

--- plotting/sma_5_8_cross.py
+++ plotting/sma_5_8_cross.py
@@ -35,7 +35,14 @@ for symbol in args.ticker:
         # Get stock data from Yahoo Finance
         data = yf.download(symbol, start='2020-01-01', interval=args.interval, progress=False, threads=True )
         data.to_csv ( '{}'.format ( args.csv_file ) )
-        
+
+    # If the csv file is older than 1440 ( 24h * 60min )        
+    today = datetime.datetime.now().date()
+    if os.path.exists(args.csv_file) and (lambda file_path: datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getmtime(args.csv_file)) > datetime.timedelta(minutes=1440))(args.csv_file):
+        # Get stock data from Yahoo Finance
+        data = yf.download(symbol, start='2020-01-01', interval=args.interval, progress=False, threads=True )
+        data.to_csv ( '{}'.format ( args.csv_file ) )
+            
     data = pd.read_csv ( args.csv_file, index_col='Date' )
 
     data["SMA_5"] = data["Adj Close"].rolling(window=5).mean()
